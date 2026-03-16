b6ff7cb7820d27397e5062aca47a12d50cc7a2df
sildpuyd <siltn.depuydt@ugent.be>
2026-03-16 11:30:43 +0100

    remove the #me

--- assets/templates/group-1/create.js
+++ assets/templates/group-1/create.js
@@ -10,7 +10,7 @@
   document.getElementById("policyType").addEventListener("change", togglePolicyType);
   togglePolicyType();
   
-  document.getElementById("provider").value = "https://AliceSmith/profile/card#me";
+  document.getElementById("provider").value = "https://AliceSmith/profile/card";
   
   // Accesible multi select
   const purposeSelect = new TomSelect('#purpose', {
